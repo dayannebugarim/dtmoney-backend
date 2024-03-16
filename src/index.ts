3e34f4c762ac27a98eb8d530c4d32acb1e086705
Dayanne Bugarim <dayannebugarim1@gmail.com>
2024-03-16 14:01:28 -0300

    :wrench: chore: project first setup

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import { config } from "dotenv";
+
+config();
+const app = express();
+
+const port = process.env.PORT || 8000;
+
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
+
+app.listen(port, () => console.log(`listening on port ${port}`));
