5a1048242c3ec57ab8e510e3bb7180b74b2cfca9
Dayanne Bugarim <dayannebugarim1@gmail.com>
2024-09-22 13:49:24 -0300

    :wrench: chore: cors config

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,28 @@
-import express from 'express';
-import { config } from 'dotenv';
-import { MongoClient } from './database/MongoClient';
-import { router } from './routes';
+import express from "express";
+import { config } from "dotenv";
+import cors from "cors";
+import { MongoClient } from "./database/MongoClient";
+import { router } from "./routes";
 
 const main = async () => {
   config();
   const app = express();
   await MongoClient.connect();
   const port = process.env.PORT || 8000;
-  
+
   app.use(express.json());
-  app.use(router)
+  app.use(
+    cors({
+      origin: "*",
+      methods: ["GET", "POST", "PUT", "DELETE"],
+      credentials: true,
+    })
+  );
+  app.use(router);
 
   app.listen(port, () => {
-      console.log(`Running at http://localhost:${port}`);
+    console.log(`Running at http://localhost:${port}`);
   });
-}
+};
 
 main();
